9b553b49d1c191ba9f4e87b6ac5044054f345d42
sahiltapu <sahilmishranayagarh@gmail.com>
2024-05-23 18:43:05 +0530

    connection to MongoDB

---
+++ db/connectToDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ConnectToMongoDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB");
+
+    }catch(error){
+        console.log("Error connecting to MongoDB " , error.message)
+    }
+}
+
+export default ConnectToMongoDB;

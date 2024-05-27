90ae48fa9822c04825a2fd0b5c4ee8021d2d6403
sahiltapu <sahilmishranayagarh@gmail.com>
2024-05-27 20:31:17 +0530

    scadcn components

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        custom:"text-[#F0F8FF] hover:text-primary text-sm lg:text-lg bg-[#915EFF] hover:bg-[#9666fb]"
+        custom:"text-[#F0F8FF] hover:text-primary text-sm bg-[#915EFF] hover:bg-[#9666fb]"
       },
       size: {
         default: "h-10 px-4 py-2",

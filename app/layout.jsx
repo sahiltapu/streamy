2109401199fc3e7c4cb5c0868c7ef81c93bee6e6
sahiltapu <sahilmishranayagarh@gmail.com>
2024-05-29 11:50:35 +0530

    added toast

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import ThemeProvider from "@/components/theme-provider";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -37,6 +38,7 @@ export default function RootLayout({ children }) {
             forcedTheme={"dark"}
             storageKey="gamehub-theme"
           >
+            <Toaster position="bottom-right" richColors expand={true} />
             {children}
           </ThemeProvider>
         </ClerkProvider>

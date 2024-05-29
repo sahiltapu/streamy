19d6404343c896e728d069a63b75e5c341f4905f
sahiltapu <sahilmishranayagarh@gmail.com>
2024-05-29 11:50:21 +0530

    modified the pages to handle the follow button

---
+++ app/(browse)/[username]/page.jsx
@@ -0,0 +1,34 @@
+import { isFollowingUser } from "../../../services/followService"
+import { getUserByUsername } from '@/services/userServces'
+import { notFound } from 'next/navigation'
+import React from 'react'
+import FollowButton from "@/components/FollowButton"
+import { getSelf } from "@/services/authService"
+
+const UserPage = async ({ params }) => {
+    const user = await getUserByUsername(params.username)
+    if (!user) {
+        notFound();
+    }
+    // console.log(user._id)
+    const isFollowing = await isFollowingUser(user._id.toString());
+    // console.log(isFollowing)
+    const loggedInUser = await getSelf();
+    // console.log(loggedInUser);
+    return (
+        <div className='flex flex-col gap-y-4'>
+            <p>username: {user.username}</p>
+            <p>userId : {String(user._id)}</p>
+            <p>userExternalId: {user.externalUserId}</p>
+            <p>isFollowing : {`${isFollowing}`}</p>
+            <FollowButton
+                loggedInUser={!loggedInUser}
+                UserId={await user._id.toString()}
+                isFollowing={isFollowing}
+
+            />
+        </div>
+    )
+}
+
+export default UserPage
